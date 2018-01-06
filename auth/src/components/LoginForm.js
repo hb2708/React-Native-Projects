b1b8d08e7d901d95d12d579b1f447ed673c62bb7
HarshalB <hb2708@gmail.com>
2018-01-06 17:12:33 +0800

    Added all The Ui components for login page

---
+++ auth/src/components/LoginForm.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Button, Card, CardSection, Input } from './common';
+
+class LoginForm extends Component {
+  state = { email: '', password: '' };
+
+  render() {
+    return (
+    <Card>
+      <CardSection>
+        <Input
+          value={this.state.email}
+          onChangeText={email => this.setState({ email })}
+          label='Email'
+          placeholder='user@gmail.com'
+        />
+      </CardSection>
+
+      <CardSection>
+        <Input
+          value={this.state.password}
+          onChangeText={password => this.setState({ password })}
+          label='Password'
+          placeholder='password'
+          secureTextEntry
+        />
+      </CardSection>
+
+      <CardSection>
+        <Button>Login</Button>
+      </CardSection>
+    </Card>
+    );
+  }
+}
+
+export default LoginForm;
