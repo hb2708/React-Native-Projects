42b61190fde5999fc0aff5ea20956f87a197803e
HarshalB <hb2708@gmail.com>
2018-01-14 23:45:44 +0800

    Added the reducer and started with email changed action

--- manager/src/App.js
+++ manager/src/App.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import firebase from 'firebase';
 import reducers from './reducers';
-import LoginForm from './components/LoginForm'
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
   componentWillMount() {
